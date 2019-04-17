efb1d7637daeeb09cbb0e8781365ff8fadb6cf29
helloShen <symantec__@hotmail.com>
2019-04-16 23:06:02 -0400

    initialize empty spring project of chapter 1

---
+++ ch01/src/main/java/com/ciaoshen/sia4/SpringInAction4thEditionDemoApplication.java
@@ -0,0 +1,13 @@
+package com.ciaoshen.sia4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringInAction4thEditionDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringInAction4thEditionDemoApplication.class, args);
+	}
+
+}
