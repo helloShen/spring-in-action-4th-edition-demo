dfe694d896dd64e8acc85cb98738dc44bacfece1
helloShen <symantec__@hotmail.com>
2019-04-23 20:08:45 -0400

    update chapter 2 example 2.2 xml autowired

---
+++ ch02/README.md
@@ -0,0 +1,16 @@
+# Spring in Action - 4th Edition - Chapter 2 Notes
+
+![spring-context](https://img.shields.io/badge/spring--context-5.1.6-brightgreen.svg) ![spring-test](https://img.shields.io/badge/spring--test-5.1.6-brightgreen.svg) ![aspectj](https://img.shields.io/badge/aspectjweaver-1.9.3-brightgreen.svg) ![junit](https://img.shields.io/badge/junit-4.12-brightgreen.svg) ![mokito](https://img.shields.io/badge/mokito--core-2.23.4-brightgreen.svg) ![hamcrest](https://img.shields.io/badge/hamcrest--core-1.3-brightgreen.svg)
+
+### 2.2 XML自动装配
+
+```
+└── com
+    └── ciaoshen
+        └── sia4
+            └── ch02
+                └── xml_autowired_22
+                    └── soundsystem
+                        ├── CompactDisc.java
+                        └── SixPense.java
+```
---
+++ ch02/src/test/java/com/ciaoshen/sia4/ch02/xml_autowired_22/soundsystem/CDPlayerTest.java
@@ -0,0 +1,25 @@
+package com.ciaoshen.sia4.ch02.xml_autowired_22.soundsystem;
+
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("classpath:/xml_autowired_22/soundsystem.xml")
+public class CDPlayerTest {
+
+    @Autowired
+    private CompactDisc cd;
+
+    @Test
+    public void cdShouldNotBeNull() {
+        assertNotNull(cd);
+        assertThat(cd.toString().equals("Playing [Life is more than six pense] by @Dajin AI"), is(true));
+    }    
+
+}
